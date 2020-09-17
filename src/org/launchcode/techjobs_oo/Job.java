0e008ca6fb216d48184954664627686e085e9952
kmwatson <katewatson@protonmail.com>
2020-09-17 12:15:05 -0500

    completed support classes

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -4,6 +4,7 @@ public class Job {
 
     private int id;
     private static int nextId = 1;
+    private String value;
 
     private String name;
     private Employer employer;
@@ -15,8 +16,34 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String aValue) {
+        this();
+        this.value = aValue;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+
+    public boolean equals(Object jobToBeCompared) {
+        if (jobToBeCompared == this) {
+            return true;
+        }
+        
+        if (jobToBeCompared == null) {
+            return false;
+        }
+
+        if (jobToBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Job theJob = (Job) jobToBeCompared;
+        return theJob.getJobId() == getJobId();
+    }
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
