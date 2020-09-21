5df2eb05ece6e241e3eaf52b4f8880f63fe8009b
kmwatson <katewatson@protonmail.com>
2020-09-21 18:42:35 -0500

    added empty constructor, full constructor and equals method unit tests

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,22 +2,51 @@ package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
 import static org.junit.Assert.*;
 
 public class JobTest {
 
+    Job firstTestJob;
+    Job secondTestJob;
+    Job constructorTestJob;
+    Job firstJobToTestEquality;
+    Job secondJobToTestEquality;
+
     @Before
-    public Job firstTestJob;
-    public Job secondTestJob;
+    public void createTestJobs(){
+        firstTestJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        secondTestJob = new Job();
+        //jobToTestEquality =
+        constructorTestJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        //firstJobToTestEquality = new Job("");
+    }
 
     @Test
     public void testSettingJobId() {
-        firstTestJob = new Job();
-        secondTestJob = new Job();
+        assertEquals(firstTestJob.getId(), 1);
+        assertEquals(secondTestJob.getId(), 2);
         assertNotEquals(firstTestJob.getId(), secondTestJob.getId());
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertEquals(constructorTestJob.getName(), "Product tester");
+        assertEquals(constructorTestJob.getEmployer().getValue(), "ACME");
+        assertEquals(constructorTestJob.getLocation().getValue(), "Desert");
+        assertEquals(constructorTestJob.getPositionType().getValue(), "Quality Control");
+        assertEquals(constructorTestJob.getCoreCompetency().getValue(), "Persistence");
+        //assertTrue(constructorTestJob instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+
+        firstJobToTestEquality = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Contorl"), new CoreCompetency("Persistence"));
+        secondJobToTestEquality = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertFalse(firstJobToTestEquality.equals(secondJobToTestEquality));
+
+    }
 
 }
